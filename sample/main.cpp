803eafc1a5080b23995ce7972576e5e21faf8414
東野祐太 <higashinoyuta@macbook-pro-3.local>
2018-12-05 01:22:24 +0900

    Initial Commit

---
+++ sample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sample
+//
+//  Created by 東野祐太 on 2018/12/05.
+//  Copyright © 2018年 東野祐太. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
